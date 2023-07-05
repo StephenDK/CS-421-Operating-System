b599910ed7ff3780eaccd22e17237942020c4a9f
StephenDK <stephendklein08@gmail.com>
2023-07-05 16:54:28 -0700

    Print Hello

--- boot_sect_simple.asm
+++ boot_sect_simple.asm
@@ -1,3 +1,18 @@
+
+; Print hello to screen
+mov ah, 0x0e ;TTY Mode
+mov al,'H'
+int 0x10
+mov al,'E'
+int 0x10 
+mov al,'L'
+int 0x10 
+int 0x10 ; L is still stored in interupt
+mov al,'O'
+int 0x10
+
+
+
 ; Infinite loop (e9 fd ff)
 loop:
     jmp loop
